93b93cb56f7c326490366648732c05c800fe71b8
Smail <ismailmansouri571@gmail.com>
2025-06-22 22:58:19 +0100

    fix cors

--- src/main/java/com/osm/gateway/GatewayApplication.java
+++ src/main/java/com/osm/gateway/GatewayApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @EnableFeignClients
 public class GatewayApplication {
 
