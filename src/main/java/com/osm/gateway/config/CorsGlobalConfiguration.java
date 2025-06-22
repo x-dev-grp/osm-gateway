4bb777184c070e93fec0b9dd0fe8f1f4c6b250db
Smail <ismailmansouri571@gmail.com>
2025-06-22 14:15:40 +0100

    fix cors

--- src/main/java/com/osm/gateway/config/CorsGlobalConfiguration.java
+++ src/main/java/com/osm/gateway/config/CorsGlobalConfiguration.java
@@ -1,18 +1,37 @@
 package com.osm.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
-public class CorsGlobalConfiguration implements WebFluxConfigurer {
+public class CorsGlobalConfiguration {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://osm-ms-fe.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowCredentials(true)
-                .maxAge(3600);
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            var request = exchange.getRequest();
+            var response = exchange.getResponse();
+
+            if (request.getHeaders().getOrigin() != null) {
+                response.getHeaders().add("Access-Control-Allow-Origin", "https://osm-ms-fe.onrender.com");
+                response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                response.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Content-Type");
+                response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            }
+
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+                return response.setComplete();
+            }
+
+            return chain.filter(exchange);
+        };
     }
 }
